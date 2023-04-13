d72f0305cde29f030e7d9ca040ba7e56e7d23331
radenlarko <radenlarko@gmail.com>
2023-04-13 14:09:34 +0700

    Add Controller getUsers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entity/user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async getUsers(): Promise<User[]> {
+    return await this.usersService.getUsers();
+  }
 
   @Post()
   async createUser(@Body() payload: CreateUserDto): Promise<void> {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entity/user.entity';
 import { UserRepository } from './repository/user.repository';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepository: UserRepository) {}
+
+  async getUsers(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
 
   async createUser(createUserDto: CreateUserDto): Promise<void> {
     return await this.userRepository.createUser(createUserDto);
