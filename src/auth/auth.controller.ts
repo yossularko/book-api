62ce57280c0b23fa2499de613a65a936a3fe7e60
radenlarko <radenlarko@gmail.com>
2023-04-14 11:16:03 +0700

    Add controller refresh-token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { RefreshAccessTokenDto } from './dto/refresh-access-token.dto';
 import { LoginRes } from './interface/login-res.interface';
+import { RefreshTokenRes } from './interface/refresh-token-res.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +12,12 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginDto: LoginDto): Promise<LoginRes> {
     return this.authService.login(loginDto);
+  }
+
+  @Post('refresh-token')
+  async refreshToken(
+    @Body() refreshAccessTokenDto: RefreshAccessTokenDto,
+  ): Promise<RefreshTokenRes> {
+    return this.authService.refreshAccessToken(refreshAccessTokenDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,18 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { refreshTokenConfig } from 'src/config/jwt.config';
 import { User } from 'src/users/entity/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { LoginDto } from './dto/login.dto';
+import { RefreshAccessTokenDto } from './dto/refresh-access-token.dto';
 import { LoginRes } from './interface/login-res.interface';
+import { RefreshTokenRes } from './interface/refresh-token-res.interface';
 import { RefreshTokenRepository } from './repository/refresh-token.repository';
+import { TokenExpiredError } from 'jsonwebtoken';
 
 @Injectable()
 export class AuthService {
@@ -28,6 +35,41 @@ export class AuthService {
     const refresh_token = await this.createRefreshToken(user);
 
     return { access_token, refresh_token };
+  }
+
+  async refreshAccessToken(
+    refreshAccessTokenDto: RefreshAccessTokenDto,
+  ): Promise<RefreshTokenRes> {
+    const { refresh_token } = refreshAccessTokenDto;
+    const payload = await this.decodeToken(refresh_token);
+    const refreshToken = await this.refereshTokenRepository.findOne({
+      where: { id: payload.jid },
+      relations: ['user'],
+    });
+
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token is not found');
+    }
+
+    if (refreshToken.isRevoked) {
+      throw new UnauthorizedException('Refresh token has been revoked');
+    }
+
+    const access_token = await this.createAccessToken(refreshToken.user);
+
+    return { access_token };
+  }
+
+  async decodeToken(token: string): Promise<any> {
+    try {
+      return await this.jwtService.verifyAsync(token);
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        throw new UnauthorizedException('Refresh token is expired!');
+      } else {
+        throw new InternalServerErrorException('Failed to decode token');
+      }
+    }
   }
 
   async createAccessToken(user: User): Promise<string> {
